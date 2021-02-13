ff6992cac161f318680fee44d0077bcc6320f680
David <daviddelgado1998@gmail.com>
2021-02-13 21:18:01 +0100

    Added image viewer and links to projects

--- main.js
+++ main.js
@@ -2,24 +2,40 @@ const navLinks = document.querySelectorAll("nav a");
 const menuIcon = document.querySelector("header div div i");
 const navBar = document.querySelector("nav");
 const main = document.querySelector("main");
+const imgs = document.querySelectorAll("main img");
+const modalImg = document.querySelector(".modal img");
+const modal = document.querySelector(".modal");
 
 //Navbar links close navbar on click
 navLinks.forEach(element => {
     element.onclick = () => {
-        navBar.classList = ["hidden"];
+        navBar.classList.add("hidden");
     }
 });
 
 //Menu icon opens and closes navbar
 menuIcon.onclick = () => {
     if (navBar.classList.length == 0) {
-        navBar.classList = ["hidden"];
+        navBar.classList.add("hidden");
     } else {
-        navBar.classList = [""];
+        navBar.classList.remove("hidden");
     }
 }
 
 //Clicking outside the navbar closes it
 main.onclick = () => {
-    navBar.classList = ["hidden"];
+    navBar.classList.add("hidden");
+}
+
+//Cliking on imgs brings up the modal
+imgs.forEach(element => {
+    element.onclick = () => {
+        modalImg.src = element.src;
+        modal.classList.remove("hidden");
+    }
+});
+
+//Clinking outside the image closes de modal
+modal.onclick = () => {
+    modal.classList.add("hidden");
 }
